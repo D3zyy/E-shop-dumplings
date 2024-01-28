e1096ecd7ffc9fa7562040f0d96fd3dede38a6c4
D3zyy <michaldolezel@gmail.com>
2024-01-28 01:52:40 +0100

    updated index page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,22 +3,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Service\UserInfoService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController {
+class IndexController extends AbstractController
+{
     private $userInfoService;
+    private $entityManager;
 
-    public function __construct(UserInfoService $userInfoService) {
+    public function __construct(UserInfoService $userInfoService, EntityManagerInterface $entityManager)
+    {
         $this->userInfoService = $userInfoService;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/', name: 'index')]
-    public function index(): Response {
+    public function index(): Response
+    {
         $userInfo = $this->userInfoService->getUserInfo();
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
 
-        return $this->render('index.html.twig', $userInfo);
+        return $this->render('index.html.twig', ['userInfo' => $userInfo, 'Products' => $products]);
     }
 }
